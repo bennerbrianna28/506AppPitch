2fa0237bd07518ed2e92b16bd86512d417e04df9
unknown <you@example.com>
2026-05-20 12:26:37 -0700

    Several updates: slider input now allows user to select a range of dates, turbidity was removed since not all guages monitor it, the dates were converted to water year, data can now be aggregated, and the plot is an interactive plotly

--- AppPitch/app.R
+++ AppPitch/app.R
@@ -2,6 +2,7 @@ library(shiny)
 library(tidyverse)
 library(ggplot2)
 library(dataRetrieval)
+library(plotly)
 
 # Define UI 
 ui <- fluidPage(
@@ -22,82 +23,128 @@ sidebarLayout(
         "USGS-12213100 (Ferndale)" = "USGS-12213100",
         "USGS-12211200 (Everson)" = "USGS-12211200",
         "USGS-12210700 (North Cedarville)" = "USGS-12210700",
-        "USGS-12210000 (South Fork)" = "USGS-12210000",
-        "USGS-12208000 (Middle Fork)" = "USGS-12208000",
-        "USGS-12205000 (North Fork)" = "USGS-12205000"
+        "USGS-12210000 (South Fork, Acme)" = "USGS-12210000",
+        "USGS-12208000 (Middle Fork, near Deming)" = "USGS-12208000",
+        "USGS-12205000 (North Fork, Glacier)" = "USGS-12205000"
       ),
       selected = "USGS-12213100"
     ),
     
+
     selectInput(
       inputId = "parameter",
       label = "Choose a variable to plot:",
       choices = c(
         "Gauge Height" = "00065",
-        "Discharge" = "00060",
-        "Turbidity" = "63680"
+        "Discharge" = "00060"
       )
     ),
+   
+  sliderInput("date_range",
+              "Select water year range:",
+              min = 1991,
+              max = 2026,
+              value=c(1991, 2026),
+              step = 1,
+              sep = ""
+),
+
+  # aggregation checkbox
+  checkboxInput(
+    inputId = "use_aggregation",
+    label = "Aggregate data",
+    value = FALSE
+  ),
+
+  conditionalPanel(
+    condition = "input.use_aggregation == true",
     
-    #dateRangeInput(
-      #inputId = "date_range",
-      #label = "Date Range:",
-      #start = "1990-10-01",
-      #end = Sys.Date()
-    #)
-  
-  sliderInput("dateSlider",
-              "Choose your start dates:",
-              min = as.Date("1990-10-01","%Y-%m-%d"),
-              max = as.Date("2026-09-30","%Y-%m-%d"),
-              value=as.Date("1990-10-01"),
-              timeFormat="%Y-%m-%d")
-    
+    selectInput(
+      inputId = "aggregation",
+      label = "Aggregation interval:",
+      choices = c(
+        "Weekly" = "week",
+        "Monthly" = "month",
+        "Yearly" = "year"
+      ),
+      selected = "week"
+    )
+  )
     
   ),
   
   # Main panel
   mainPanel(
-    plotOutput("my_plot"),
+    plotlyOutput("my_plot"),
     )
   )
 )
 
-# Define server logic required to draw a histogram
 
-# to revert back to the dateRangeInput, replace dateSlider with date_range
 server <- function(input, output) {
-  # reactive to get the data
+  # reactive to get the raw data
   water_data <- reactive({
-    req(input$gauges, input$dateSlider, input$parameter) # inputs
+    req(input$gauges, input$date_range, input$parameter) # inputs
+    
+    # convert water years to dates
+    start_date <- as.Date(
+      paste0(input$date_range[1] - 1, "-10-01")
+    )
+    
+    end_date <- as.Date(
+      paste0(input$date_range[2], "-09-30")
+    )
     
     # daily values retrieval
     read_waterdata_daily(
       monitoring_location_id = input$gauges,
       parameter_code = input$parameter,
-      time = c(input$dateSlider[1], input$dateSlider[2]),
+      time = c(start_date, end_date),
     )
   })
   
-  parameter_labels <- c(
-    "00060" = "Discharge (cfs)",
-    "00065" = "Gauge Height (ft)",
-    "63680" = "Turbidity (NTU)"
-  )
-  
-  # Plot
-  output$my_plot <- renderPlot({
+  # aggregate the data if requested
+  plot_data <- reactive({
     
     df <- water_data()
     
-    # identify value column
-    value_col <- "value"
+    # no aggregation
+    if (!input$use_aggregation) {
+      
+      df$period <- df$time
+      return(df)
+      
+    }
+    
+    # aggregate data
+    df %>%
+      mutate(
+        period = floor_date(time, unit = input$aggregation)
+      ) %>%
+      group_by(monitoring_location_id, period) %>%
+      summarize(
+        value = mean(value, na.rm = TRUE),
+        .groups = "drop"
+      )
+    
+  })  
+  
+  # axis labels
+  parameter_labels <- c(
+    "00060" = "Discharge (cfs)",
+    "00065" = "Gauge Height (ft)"
+  )
+  
+  # Plot
+  output$my_plot <- renderPlotly({
+    
+    df <- plot_data()
     
     ggplot(
       df,
       aes(
-        x = time,
-        y = .data[[value_col]],
+        x = period,
+        y = value,
         color = monitoring_location_id
       )
     ) +
@@ -107,13 +154,12 @@ server <- function(input, output) {
         y = parameter_labels[input$parameter],
         color = "Gauge"
       ) +
+      scale_color_viridis_d() +
       theme_bw()
     
   })
-    
 }
 
-  
 
 
 # Run the application 
